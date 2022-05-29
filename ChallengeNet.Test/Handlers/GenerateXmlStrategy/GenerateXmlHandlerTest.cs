d669c406d6bebfab74b8da4f2f840e67f13a07d1
lkrjunior <lkrjunior@terra.com.br>
2022-05-29 16:32:43 -0300

    GenerateXmlHandlerTest class

---
+++ ChallengeNet.Test/Handlers/GenerateXmlStrategy/GenerateXmlHandlerTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using ChallengeNet.Core.Handlers.GenerateXmlStrategy;
+using ChallengeNet.Core.Models;
+using Xunit;
+
+namespace ChallengeNet.Test.Handlers.GenerateXmlStrategy
+{
+    public class GenerateXmlHandlerTest
+    {
+        [Fact]
+        public void ShouldGenerateXmlNfeWhenInvokeMethod()
+        {
+            #region Arrange
+
+            var expectedElement = nameof(ProductType.Nfe);
+
+            var handler = new GenerateXmlHandler();
+
+            #endregion
+
+            #region Act
+
+            var result = handler.GenerateXml(ProductType.Nfe);
+
+            #endregion
+
+            #region Assert
+
+            var element = result.Root.Element(nameof(ProductType.Nfe)).Name;
+
+            Assert.Equal(expectedElement, element);
+
+            #endregion
+        }
+
+        [Fact]
+        public void ShouldGenerateXmlNfceWhenInvokeMethod()
+        {
+            #region Arrange
+
+            var expectedElement = nameof(ProductType.Nfce);
+
+            var handler = new GenerateXmlHandler();
+
+            #endregion
+
+            #region Act
+
+            var result = handler.GenerateXml(ProductType.Nfce);
+
+            #endregion
+
+            #region Assert
+
+            var element = result.Root.Element(nameof(ProductType.Nfce)).Name;
+
+            Assert.Equal(expectedElement, element);
+
+            #endregion
+        }
+    }
+}
