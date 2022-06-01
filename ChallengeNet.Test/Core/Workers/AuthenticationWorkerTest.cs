fd1e740df18fe8648a9d1124bb6609f424b628d1
Luciano Junior Kruse Rocha <luciano.rocha@linx.com.br>
2022-06-01 09:06:47 -0300

    Rename

---
+++ ChallengeNet.Test/Core/Workers/AuthenticationWorkerTest.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Threading.Tasks;
+using ChallengeNet.Core.Core.Workers;
+using ChallengeNet.Core.Interfaces;
+using ChallengeNet.Core.Models.Response;
+using ChallengeNet.Core.Models.User;
+using ChallengeNet.Test.Fake;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace ChallengeNet.Test.Core.Workers
+{
+    public class AuthenticationWorkerTest
+    {
+        [Fact]
+        public async Task ShouldCreateAccessTokenWhenUserIsValid()
+        {
+            #region Arrange
+
+            var userName = "username";
+            var password = "password";
+            var role = "admin";
+
+            var expectedAuthenticationResponse = new AuthenticationResponse()
+            {
+                AccessToken = Guid.NewGuid().ToString(),
+                ExpiresAtUtc = DateTime.UtcNow.AddMinutes(10)
+            };
+            
+            var user = UserFake.GenerateUser(userName, password);
+
+            var userFromRepository = UserFake.GenerateUser(userName, password, role);
+
+            var userRepository = new Mock<IUserRepository>();
+            var currentUserName = default(string);
+            userRepository.Setup(x => x.GetAsync(It.IsAny<string>()))
+                .ReturnsAsync((string userName) =>
+                {
+                    currentUserName = userName;
+
+                    return userFromRepository;
+                });
+
+            var userTokenHandler = new Mock<IUserTokenHandler>();
+            userTokenHandler.Setup(x => x.GenerateAccessToken(userFromRepository))
+                .Returns(expectedAuthenticationResponse);
+
+            var logger = new Mock<ILogger<AuthenticationWorker>>();
+
+            var authenticationCore = new AuthenticationWorker(userRepository.Object, userTokenHandler.Object, logger.Object);
+
+            #endregion
+
+            #region Act
+
+            var result = await authenticationCore.ExecuteAsync(user);
+
+            #endregion
+
+            #region Assert
+
+            userRepository.Verify(x => x.GetAsync(userName), Times.Once);
+
+            userTokenHandler.Verify(x => x.GenerateAccessToken(userFromRepository), Times.Once);
+
+            Assert.Equal(currentUserName, userName);
+            Assert.False(result.HasError);
+            Assert.NotNull(result.Data);
+            Assert.Equal(expectedAuthenticationResponse, result.Data);
+
+            #endregion
+        }
+
+        [Fact]
+        public async Task ShouldNotCreateAccessTokenWhenUserIsInvalid()
+        {
+            #region Arrange
+
+            var userName = "username";
+            var password = "password";
+            var expectedAccessToken = Guid.NewGuid().ToString();
+            var expectedStatusCode = StatusCodes.Status400BadRequest;
+
+            var user = UserFake.GenerateUser(userName, password);
+
+            var userFromRepository = default(User);
+
+            var userRepository = new Mock<IUserRepository>();
+            userRepository.Setup(x => x.GetAsync(userName)).ReturnsAsync(userFromRepository);
+
+            var userTokenHandler = new Mock<IUserTokenHandler>();
+            
+            var logger = new Mock<ILogger<AuthenticationWorker>>();
+
+            var authenticationCore = new AuthenticationWorker(userRepository.Object, userTokenHandler.Object, logger.Object);
+
+            #endregion
+
+            #region Act
+
+            var result = await authenticationCore.ExecuteAsync(user);
+
+            #endregion
+
+            #region Assert
+
+            userRepository.Verify(x => x.GetAsync(userName), Times.Once);
+
+            userTokenHandler.Verify(x => x.GenerateAccessToken(userFromRepository), Times.Never);
+
+            Assert.True(result.HasError);
+            Assert.Equal(expectedStatusCode, result.HttpStatusCode);
+            Assert.NotNull(result.ErrorMessage);
+
+            #endregion
+        }
+
+        [Fact]
+        public async Task ShouldReturnInternalServerErrorWhenThrowsAnException()
+        {
+            #region Arrange
+
+            var userName = "username";
+            var password = "password";
+            var expectedAccessToken = Guid.NewGuid().ToString();
+            var expectedStatusCode = StatusCodes.Status500InternalServerError;
+
+            var user = UserFake.GenerateUser(userName, password);
+
+            var userFromRepository = default(User);
+
+            var userRepository = new Mock<IUserRepository>();
+            userRepository.Setup(x => x.GetAsync(userName)).Throws(new Exception("Cannot access repository"));
+
+            var userTokenHandler = new Mock<IUserTokenHandler>();
+            
+            var logger = new Mock<ILogger<AuthenticationWorker>>();
+
+            var authenticationCore = new AuthenticationWorker(userRepository.Object, userTokenHandler.Object, logger.Object);
+
+            #endregion
+
+            #region Act
+
+            var result = await authenticationCore.ExecuteAsync(user);
+
+            #endregion
+
+            #region Assert
+
+            userRepository.Verify(x => x.GetAsync(userName), Times.Once);
+
+            userTokenHandler.Verify(x => x.GenerateAccessToken(userFromRepository), Times.Never);
+
+            logger.Verify(x => x.Log(LogLevel.Error, It.IsAny<EventId>(), It.IsAny<It.IsAnyType>(), It.IsAny<Exception>(), It.IsAny<Func<It.IsAnyType, Exception, string>>()), Times.Once);
+
+            Assert.True(result.HasError);
+            Assert.Equal(expectedStatusCode, result.HttpStatusCode);
+            Assert.NotNull(result.ErrorMessage);
+
+            #endregion
+        }
+    }
+}
---
+++ ChallengeNet.Test/Core/Workers/RegisterPessoaJuridicaWorkerTest.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Threading.Tasks;
+using ChallengeNet.Core.Core.Workers;
+using ChallengeNet.Core.Interfaces;
+using ChallengeNet.Core.Models.Register;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace ChallengeNet.Test.Core.Workers
+{
+    public class RegisterPessoaJuridicaWorkerTest
+    {
+        [Fact]
+        public async Task ShouldCreatePessoaJuridicaWhenPayloadIsValid()
+        {
+            #region Arrange
+
+            var pessoa = new PessoaJuridica()
+            {
+                Name = "Name",
+                Cnpj = "12345678901234"
+            };
+
+            var repositoryMock = new Mock<IPessoaRepository<PessoaJuridica>>();
+            repositoryMock.Setup(x => x.Create(It.IsAny<PessoaJuridica>())).ReturnsAsync(true);
+
+            var loggerMock = new Mock<ILogger<RegisterPessoaJuridicaWorker>>();
+
+            var worker = new RegisterPessoaJuridicaWorker(repositoryMock.Object, loggerMock.Object);
+            #endregion
+
+            #region Act
+
+            var result = await worker.Create(pessoa);
+
+            #endregion
+
+            #region Assert
+
+            repositoryMock.Verify(x => x.Create(It.IsAny<PessoaJuridica>()), Times.Once);
+
+            Assert.False(result.HasError);
+            Assert.NotNull(result.Data);
+
+            #endregion
+        }
+
+        [Fact]
+        public async Task ShouldNotCreatePessoaJuridicaWhenPayloadIsInvalid()
+        {
+            #region Arrange
+
+            var expectedHttpStatusCode = StatusCodes.Status400BadRequest;
+
+            var pessoa = new PessoaJuridica();
+
+            var repositoryMock = new Mock<IPessoaRepository<PessoaJuridica>>();
+
+            var loggerMock = new Mock<ILogger<RegisterPessoaJuridicaWorker>>();
+
+            var worker = new RegisterPessoaJuridicaWorker(repositoryMock.Object, loggerMock.Object);
+            #endregion
+
+            #region Act
+
+            var result = await worker.Create(pessoa);
+
+            #endregion
+
+            #region Assert
+
+            repositoryMock.Verify(x => x.Create(It.IsAny<PessoaJuridica>()), Times.Never);
+
+            Assert.True(result.HasError);
+            Assert.Equal(expectedHttpStatusCode, result.HttpStatusCode);
+            Assert.NotNull(result.ErrorMessage);
+
+            #endregion
+        }
+
+        [Fact]
+        public async Task ShouldReturnsInternalServerErrorOnCreateWhenThrowsAnExceprion()
+        {
+            #region Arrange
+
+            var expectedHttpStatusCode = StatusCodes.Status500InternalServerError;
+
+            var pessoa = new PessoaJuridica()
+            {
+                Name = "Name",
+                Cnpj = "12345678901234"
+            };
+
+            var repositoryMock = new Mock<IPessoaRepository<PessoaJuridica>>();
+            repositoryMock.Setup(x => x.Create(It.IsAny<PessoaJuridica>())).ThrowsAsync(new Exception());
+
+            var loggerMock = new Mock<ILogger<RegisterPessoaJuridicaWorker>>();
+
+            var worker = new RegisterPessoaJuridicaWorker(repositoryMock.Object, loggerMock.Object);
+            #endregion
+
+            #region Act
+
+            var result = await worker.Create(pessoa);
+
+            #endregion
+
+            #region Assert
+
+            repositoryMock.Verify(x => x.Create(It.IsAny<PessoaJuridica>()), Times.Once);
+
+            Assert.True(result.HasError);
+            Assert.Equal(expectedHttpStatusCode, result.HttpStatusCode);
+            Assert.NotNull(result.ErrorMessage);
+
+            #endregion
+        }
+
+        [Fact]
+        public async Task ShouldFindPessoaJuridicaWhenSearchIsCorrect()
+        {
+            #region Arrange
+
+            var cnpj = "12345678901234";
+
+            var pessoa = new PessoaJuridica()
+            {
+                Name = "Name",
+                Cnpj = cnpj
+            };
+
+            var repositoryMock = new Mock<IPessoaRepository<PessoaJuridica>>();
+            repositoryMock.Setup(x => x.Find(It.IsAny<string>()))
+                .ReturnsAsync(pessoa);
+
+            var loggerMock = new Mock<ILogger<RegisterPessoaJuridicaWorker>>();
+
+            var worker = new RegisterPessoaJuridicaWorker(repositoryMock.Object, loggerMock.Object);
+            #endregion
+
+            #region Act
+
+            var result = await worker.Find(cnpj);
+
+            #endregion
+
+            #region Assert
+
+            repositoryMock.Verify(x => x.Create(It.IsAny<PessoaJuridica>()), Times.Never);
+            repositoryMock.Verify(x => x.Find(It.IsAny<string>()), Times.Once);
+
+            Assert.False(result.HasError);
+            Assert.NotNull(result.Data);
+
+            #endregion
+        }
+
+        [Fact]
+        public async Task ShouldNotFindPessoaJuridicaWhenSearchIsIncorrect()
+        {
+            #region Arrange
+
+            var expectedHttpStatusCode = StatusCodes.Status404NotFound;
+
+            var cnpj = "12345678901234";
+
+            var pessoa = new PessoaJuridica()
+            {
+                Name = "Name",
+                Cnpj = cnpj
+            };
+
+            var repositoryMock = new Mock<IPessoaRepository<PessoaJuridica>>();
+
+            var loggerMock = new Mock<ILogger<RegisterPessoaJuridicaWorker>>();
+
+            var worker = new RegisterPessoaJuridicaWorker(repositoryMock.Object, loggerMock.Object);
+            #endregion
+
+            #region Act
+
+            var result = await worker.Find(cnpj);
+
+            #endregion
+
+            #region Assert
+
+            repositoryMock.Verify(x => x.Create(It.IsAny<PessoaJuridica>()), Times.Never);
+            repositoryMock.Verify(x => x.Find(It.IsAny<string>()), Times.Once);
+
+            Assert.True(result.HasError);
+            Assert.Equal(expectedHttpStatusCode, result.HttpStatusCode);
+            Assert.NotNull(result.ErrorMessage);
+
+            #endregion
+        }
+
+        [Fact]
+        public async Task ShouldReturnsInternalServerErrorOnFindWhenThrowsAnException()
+        {
+            #region Arrange
+
+            var expectedHttpStatusCode = StatusCodes.Status500InternalServerError;
+
+            var cnpj = "12345678901234";
+
+            var pessoa = new PessoaJuridica()
+            {
+                Name = "Name",
+                Cnpj = cnpj
+            };
+
+            var repositoryMock = new Mock<IPessoaRepository<PessoaJuridica>>();
+            repositoryMock.Setup(x => x.Find(It.IsAny<string>()))
+                .ThrowsAsync(new Exception());
+
+            var loggerMock = new Mock<ILogger<RegisterPessoaJuridicaWorker>>();
+
+            var worker = new RegisterPessoaJuridicaWorker(repositoryMock.Object, loggerMock.Object);
+            #endregion
+
+            #region Act
+
+            var result = await worker.Find(cnpj);
+
+            #endregion
+
+            #region Assert
+
+            repositoryMock.Verify(x => x.Create(It.IsAny<PessoaJuridica>()), Times.Never);
+            repositoryMock.Verify(x => x.Find(It.IsAny<string>()), Times.Once);
+
+            Assert.True(result.HasError);
+            Assert.Equal(expectedHttpStatusCode, result.HttpStatusCode);
+            Assert.NotNull(result.ErrorMessage);
+
+            #endregion
+        }
+    }
+}
